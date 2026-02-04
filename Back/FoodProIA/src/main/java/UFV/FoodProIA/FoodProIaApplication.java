3c698e3a098bf51a45753b50891ef4206aa9d93d
Heitor <hectorpovov@gmail.com>
2026-02-04 11:00:40 -0300

    adição da pasta do backend

---
+++ Back/FoodProIA/src/main/java/UFV/FoodProIA/FoodProIaApplication.java
@@ -0,0 +1,13 @@
+package UFV.FoodProIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodProIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodProIaApplication.class, args);
+	}
+
+}
---
+++ Back/FoodProIA/src/test/java/UFV/FoodProIA/FoodProIaApplicationTests.java
@@ -0,0 +1,13 @@
+package UFV.FoodProIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodProIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
